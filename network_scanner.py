0c6a2d512544f62fd4c3e2e18a198df9b4312eac
AsBaZa <asier.badiola@outlook.es>
2020-08-17 14:08:36 -0400

    Añadir `broadcast`

--- network_scanner.py
+++ network_scanner.py
@@ -3,8 +3,13 @@ import scapy.all as scapy
 
 
 def scan(ip):
-    scapy.arping(ip)
+    arp_request = scapy.ARP(pdst=ip)
+    broadcast = scapy.Ether(dst='ff:ff:ff:ff:ff:ff')
+    arp_request_broadcast = broadcast/arp_request
+    print(arp_request.show())
+    print(broadcast.show())
+    print(arp_request_broadcast.show())
 
 
 if __name__ == '__main__':
-    scan("10.0.2.1/24")
+    scan('10.0.2.1/24')
